7034083143077ecb60aefd83001eb1a894ef0a40
Glitch (canyon-magical-birch) <none>
2021-06-07 22:26:58 +0000

    🚂🐌 Checkpoint ./bot.js:31079087/909 ./komutlar/test.js:31079087/345

--- bot.js
+++ bot.js
@@ -1,20 +1,23 @@
 const Discord = require('discord.js'); // discord.js modülü tanımlıyoruz.
 const client = new Discord.Client();
+const Client = new Discord.Client();
 const { readdirSync } = require('fs');
 const { join } = require('path');
 const { MessageEmbed } = 'discord.js';
 const express = require('express');
 const db = require('quick.db');
 const fs = require('fs');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-const log = message => {
-  console.log(` ${message}`);
-};
 
+client.commands= new Discord.Collection(); // komutları alıyoruz
+
+const commandFiles = readdirSync(join(__dirname, "komutlar")).filter(file => file.endsWith(".js")); // Belli bir klasörden belli .js uzantılı dosyaları buluyor.
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "komutlar", `${file}`));
+    client.commands.set(command.kod, command); // Komutları Ayarlıyoruz.
+}
+
+client.on("error", console.error);
 //--------------------------------------------------//
  
 //--------------------------------------------------//
@@ -302,10 +305,16 @@ client.on('message', async message => {
 		const command = args.shift().toLowerCase();
 		if (!client.commands.has(command))
 			return message.channel.send(
-				`Komut dosyamda **${command}** adlı bir komut bulamadım.`
-			);
-	}
-});
+				`Komut dosyamda **${command}** adlı bir komut bulamadım.`);
+try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error){
+            console.error(error);
+        }
+    }
+})
 
 /////////
 client.login(process.env.TOKEN);
+Client.login(process.env.Token);
